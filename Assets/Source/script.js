57dca3b23f23d093977fe598fa9dbf4ea7e33dab
Andreick <andreick.gomes.ismart@gmail.com>
2021-01-31 23:48:14 -0300

    Add cactus movement

--- Assets/Source/script.js
+++ Assets/Source/script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 let isJumping = false;
 
 const handleKeyUp = event => {
@@ -38,4 +39,29 @@ const jump = () => {
     }, 20);
 }
 
+const createCactus = () => {
+    const cactus = document.createElement("div");
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add("cactus");
+    cactus.style.left = 1000 + "px";
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+
+        if (cactusPosition <= -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }
+        else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + "px";
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 document.addEventListener("keyup", handleKeyUp);
